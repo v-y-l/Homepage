64d18ee500768b07cdf939c401ff679d895a1834
Victor Lin <vlinvlin6@gmail.com>
2020-06-13 15:52:01 -0400

    Remove some fat

--- src/app/pages/article/article.ts
+++ src/app/pages/article/article.ts
@@ -1,14 +1,7 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Router } from '@angular/router';
 
 @Component({
     selector: 'article',
     templateUrl: './article.ng.html',
 })
-export class Article {
-    constructor(
-	private router: Router,
-    ) {
-    }
-}
+export class Article {}
