50b0f127240ade3f259d9596c5b811f81e53cf26
Victor Lin <vlinvlin6@gmail.com>
2020-06-14 13:53:05 -0400

    Complete basic

--- src/app/components/viz/systemic_racism/systemic_racism.ts
+++ src/app/components/viz/systemic_racism/systemic_racism.ts
@@ -19,7 +19,7 @@ export class SystemicRacismViz implements AfterViewInit {
     readonly START_Y = 20;
     // This is the top-left corner of the system.
     readonly SYSTEM_X = 20;
-    readonly SYSTEM_Y = 40;
+    readonly SYSTEM_Y = 100;
     
     svgContainer;
     rect;
@@ -28,8 +28,8 @@ export class SystemicRacismViz implements AfterViewInit {
 	this.svgContainer = d3.select("svg.viz");
 	let svgWidth = parseInt(this.svgContainer.style("width"));
 	let svgHeight = parseInt(this.svgContainer.style("height"));
-	const maxCols = (svgWidth - this.SYSTEM_Y) / (this.BLOCK_WIDTH + this.GAP_LENGTH);
-	const maxRows = (svgHeight - this.SYSTEM_X) / (this.BLOCK_HEIGHT + this.GAP_LENGTH);
+	const maxCols = (svgWidth - this.SYSTEM_Y - this.BLOCK_HEIGHT - this.GAP_LENGTH) / (this.BLOCK_WIDTH + this.GAP_LENGTH);
+	const maxRows = (svgHeight - this.SYSTEM_X - this.BLOCK_WIDTH - this.GAP_LENGTH) / (this.BLOCK_HEIGHT + this.GAP_LENGTH);
 	this.buildSystem(maxRows, maxCols);
     }
 
