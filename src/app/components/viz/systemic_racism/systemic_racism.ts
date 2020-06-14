4814dfce858ae060c97146385e1bcb0886449608
Victor Lin <vlinvlin6@gmail.com>
2020-06-14 16:08:47 -0400

    Use dataset

--- src/app/components/viz/systemic_racism/systemic_racism.ts
+++ src/app/components/viz/systemic_racism/systemic_racism.ts
@@ -2,6 +2,12 @@ import { Component, AfterViewInit } from '@angular/core';
 
 import * as d3 from 'd3';
 
+interface Point {
+    x: number,
+    y: number,
+    delay: number,
+};
+
 @Component({
     selector: 'systemic-racism-viz',
     templateUrl: './systemic_racism.ng.html',
@@ -25,64 +31,62 @@ export class SystemicRacismViz implements AfterViewInit {
     svgHeight;
     svgContainer;
     rect;
+    data: Point[] = [];
 
     ngAfterViewInit() {
 	this.svgContainer = d3.select("svg.viz");
 	this.svgWidth = parseInt(this.svgContainer.style("width")) - this.SYSTEM_X;
 	this.svgHeight = parseInt(this.svgContainer.style("height"));
 	this.buildFullPageSystem();
-	d3.select(window).on(
-	    'resize', ()=>{
-		const originalArea = this.svgWidth * this.svgHeight;
-		const newArea = (parseInt(this.svgContainer.style("width")) - this.SYSTEM_X)*parseInt(this.svgContainer.style("height"));
-		const ratio = newArea / originalArea;
-		d3.selectAll("rect")
-		    .attr("height", this.BLOCK_HEIGHT*ratio)
-		    .attr("width", this.BLOCK_WIDTH*ratio);
-		//Need to update this to shrink the gap, use a dataset instead
-		    // .attr("x", (d) => {this.x.animVal.value - this.GAP_LENGTH + this.GAP_LENGTH*ratio})
-		    // .attr("y", (d) => {this.y.animVal.value - this.GAP_LENGTH + this.GAP_LENGTH*ratio})
-	    }
+	 d3.select(window).on(
+	     'resize', ()=>{
+	 	const originalArea = this.svgWidth * this.svgHeight;
+	 	const newArea = (parseInt(this.svgContainer.style("width")) - this.SYSTEM_X)*parseInt(this.svgContainer.style("height"));
+	 	const ratio = newArea / originalArea;
+	 	d3.selectAll("rect")
+	 	    .attr("height", this.BLOCK_HEIGHT*ratio)
+	 	    .attr("width", this.BLOCK_WIDTH*ratio);
+	 	     .attr("x", (d) => {d.x - this.GAP_LENGTH + this.GAP_LENGTH*ratio})
+	 	     .attr("y", (d) => {d.y - this.GAP_LENGTH + this.GAP_LENGTH*ratio})
+	     }
 	);
     }
 
     buildFullPageSystem() {
 	const maxCols = (this.svgWidth - this.SYSTEM_X - this.BLOCK_WIDTH) / (this.BLOCK_WIDTH + this.GAP_LENGTH);
 	const maxRows = (this.svgHeight - this.SYSTEM_Y - this.BLOCK_HEIGHT - this.GAP_LENGTH) / (this.BLOCK_HEIGHT + this.GAP_LENGTH);
-	this.buildSystem(maxRows, maxCols);
+	this.initializeData(maxRows, maxCols);
+	let sel = this.svgContainer
+	    .selectAll("rect")
+	    .data(this.data);
+	sel.enter()
+	    .append("rect")
+	    .attr("x", this.START_X)
+	    .attr("y", this.START_Y)
+	    .attr("width", this.BLOCK_WIDTH)
+	    .attr("height", this.BLOCK_HEIGHT)
+	    .attr("opacity", 0)
+	    .transition()
+	    .delay(d => d.delay)
+	    .duration(this.DURATION)
+	    .attr("x", d => d.x)
+	    .attr("y", d => d.y)
+	    .attr("opacity", 1);
+	sel.exit();
     }
 
     // Builds a grid of rows x cols blocks.
-    buildSystem(rows: number, cols: number) {
+    initializeData(rows: number, cols: number) {
 	let delay = this.DELAY;
 	let x = this.SYSTEM_X;
 	for (let c = 0; c < cols; c++) {
 	    let y = this.SYSTEM_Y;
 	    for (let r = 0; r < rows; r++) {
-		this.addBlock(x, y, delay);
+		this.data.push({x, y, delay});
 		y += this.BLOCK_HEIGHT + this.GAP_LENGTH;
 		delay += this.DELAY;
 	    }
 	    x += this.BLOCK_WIDTH + this.GAP_LENGTH;
 	}
-    }
-
-    // Adds a single block from source (20,10) to
-    // destination (x,y) after the delay.
-    addBlock(x: number, y: number, delay: number) {
-	this.rect = this.svgContainer.append("rect")
-	    .attr("x", this.START_X)
-	    .attr("y", this.START_Y)
-	    .attr("width", this.BLOCK_WIDTH)
-	    .attr("height", this.BLOCK_HEIGHT)
-	    .attr("opacity", 0);
-
-	this.rect
-	    .transition()
-	    .delay(delay)
-	    .duration(this.DURATION)
-	    .attr("x", x)
-	    .attr("y", y)
-	    .attr("opacity", 1);
     }
 }
