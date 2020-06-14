8429deaf42e5299dce3f84141aabc1263dc3dde3
Victor Lin <vlinvlin6@gmail.com>
2020-06-14 13:17:18 -0400

    Work more on viz

--- src/app/components/viz/systemic_racism/systemic_racism.ts
+++ src/app/components/viz/systemic_racism/systemic_racism.ts
@@ -13,27 +13,26 @@ export class SystemicRacismViz implements AfterViewInit {
 
     ngAfterViewInit() {
 	this.svgContainer = d3.select("svg.viz");
-	this.initializeRectangles();
+	this.addBlock(20, 200, 500);
+	this.addBlock(20, 300, 1000);
     }
 
-    initializeRectangles() {
+    // x is the destination x, y is the destination y
+    // delay is the time until transition
+    addBlock(x, y, delay) {
 	this.rect = this.svgContainer.append("rect")
-	    .attr("x", 100)
-	    .attr("y", 100)
+	    .attr("x", 20)
+	    .attr("y", 10)
 	    .attr("width", 50)
 	    .attr("height", 50)
 	    .attr("opacity", 0);
 
-	this.move();
-    }
-
-    move() {
 	this.rect
 	    .transition()
-	    .delay(500)
+	    .delay(delay)
 	    .duration(1500)
-	    .attr("x", 100)
-	    .attr("y", 200)
+	    .attr("x", x)
+	    .attr("y", y)
 	    .attr("opacity", 1);
     }
 }
