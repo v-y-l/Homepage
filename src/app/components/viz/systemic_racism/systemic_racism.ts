eb1e5a84205544c8788c30f2561cdcda95a272b6
Victor Lin <vlinvlin6@gmail.com>
2020-06-19 16:16:56 -0400

    Abbreviate citations

--- src/app/components/viz/systemic_racism/systemic_racism.ts
+++ src/app/components/viz/systemic_racism/systemic_racism.ts
@@ -170,7 +170,7 @@ export class SystemicRacismViz implements AfterViewInit {
     openDialog(d, i) {
 	const dialogRef = this.dialog.open(SystemicRacismDialog, {
 	    width: '80%',
-	    height: '70%',
+	    maxHeight: '70%',
 	    data: {
 		index: this.events.length - i - 1,
 		events: this.events,
