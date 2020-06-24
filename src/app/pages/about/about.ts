08e151cc80c4ad6b3da85d0a1fbb73126f9593ea
Victor Lin <vlinvlin6@gmail.com>
2020-06-23 23:03:57 -0400

    wtv

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -31,11 +31,11 @@ export class About implements OnInit {
     }
 
     goToTaipei() {
-	this.map.setView(
-	    new ol.View({
-		center: ol.proj.fromLonLat([25.0330, 121.121.5654]),
-		zoom: 8,
-	    });
-	);
+	const tpView = new ol.View({
+	    center: ol.proj.fromLonLat([25.0330, 121.121.5654]),
+	    zoom: 8,
+	});
+
+	this.map.setView(tpView);
     }
 }
